ae3bac17b8305364679f2bbee5965ac0f581edec
NwaforAugustine321 <nwaforaugustine422@gmail.com>
2022-11-30 14:57:45 +0100

    login error fixed

--- src/utils/MkdSDK.jsx
+++ src/utils/MkdSDK.jsx
@@ -106,7 +106,7 @@ export default function MkdSDK() {
         const _jsonPostData = await response.json();
 
         if (response.status != 200) {
-        alert(response.message);
+        alert('Request failed please your login credentials');
         }
 
         return _jsonPostData;
